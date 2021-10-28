13efb9ccd47e6355f1d88836c164c6037afdf59c
Hridoy <hridoykhondokar01@gmail.con.com>
2021-10-28 21:56:48 +0600

    sass-practice

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import Header from './component/Header/Header';
+import Home from './component/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <button >SASS PRACTICE</button>
+            <p className='text'>this is header container</p> 
+              
+        </div>
+
+    );
+};
+
+export default Header;
